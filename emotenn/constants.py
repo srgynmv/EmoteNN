b4f3707201c1912ec1cd9e306230040e3fad112b
Sergei Neimushchev <srgynmv@gmail.com>
2020-01-08 16:08:17 +0300

    Move all scripts to emotenn directory

--- emotenn/constants.py
+++ emotenn/constants.py
@@ -1,12 +1,12 @@
 import os
 
 # paths section
-THIS_DIR = os.path.abspath(os.path.dirname(__file__))
-DATASETS_DIR = os.path.join(THIS_DIR, 'datasets')
+PROJECT_ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+DATASETS_DIR = os.path.join(PROJECT_ROOT_DIR, 'datasets')
 UNPACKED_DIR = os.path.join(DATASETS_DIR, 'unpacked')
-PREPROCESSED_DATA_DIR = os.path.join(THIS_DIR, 'data')
-MODELS_DIR = os.path.join(THIS_DIR, 'models')
-TRAINED_MODELS_DIR = os.path.join(THIS_DIR, 'trained_models')
+PREPROCESSED_DATA_DIR = os.path.join(PROJECT_ROOT_DIR, 'data')
+MODELS_DIR = os.path.join(PROJECT_ROOT_DIR, 'models')
+TRAINED_MODELS_DIR = os.path.join(PROJECT_ROOT_DIR, 'trained_models')
 
 TRAIN_X = os.path.join(PREPROCESSED_DATA_DIR, 'train_x.npy')
 TRAIN_Y = os.path.join(PREPROCESSED_DATA_DIR, 'train_y.npy')
