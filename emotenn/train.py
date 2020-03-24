cfbd9f5e6d951ae40e9671b855ec21fede7c668d
Sergei Neimushchev <srgynmv@gmail.com>
2020-03-25 01:24:36 +0300

    Get rid of test.py
    Code from test.py is added to the train_mode.ipynb.
    `split_dataset` function is added to  train.py.

--- emotenn/train.py
+++ emotenn/train.py
@@ -2,9 +2,10 @@ import os
 import pickle
 from . import constants
 from tensorflow.keras import callbacks
+from sklearn.model_selection import train_test_split
 
 
-def get_callbacks(model_filename):
+def get_callbacks(model_name):
     if not os.path.isdir(constants.CHECKPOINTS_DIR):
         os.mkdir(constants.CHECKPOINTS_DIR)
 
@@ -14,15 +15,21 @@ def get_callbacks(model_filename):
     return cb_list
 
 
-def save_results(model, history, model_filename):
+def split_dataset(data_x, data_y, valid_size, test_size):
+    x_train, x_test, y_train, y_test = train_test_split(data_x, data_y, test_size=(test_size + valid_size))
+    x_valid, x_test, y_valid, y_test = train_test_split(x_test, y_test, test_size=test_size / (test_size + valid_size)) 
+    return x_train, x_valid, x_test, y_train, y_valid, y_test
+
+
+def save_results(model, history, model_name):
     if not os.path.isdir(constants.TRAINED_MODELS_DIR):
         os.mkdir(constants.TRAINED_MODELS_DIR)
 
-    model_file_path = os.path.join(constants.TRAINED_MODELS_DIR, model_filename) + '.h5'
+    model_file_path = os.path.join(constants.TRAINED_MODELS_DIR, model_name) + '.h5'
     model.save(model_file_path)
     print('Model is saved to {}'.format(model_file_path))
 
-    history_path = os.path.join(constants.TRAINED_MODELS_DIR, model_filename) + '_history.pkl'
+    history_path = os.path.join(constants.TRAINED_MODELS_DIR, model_name) + '_history.pkl'
     with open(history_path, 'wb') as history_file:
         pickle.dump(history.history, history_file)
     print('Train history is saved to {}'.format(history_path))
