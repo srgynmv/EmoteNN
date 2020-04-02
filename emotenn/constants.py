6f23f11ec5a550526947b691e7723ca447f90640
Sergei Neimushchev <srgynmv@gmail.com>
2020-04-03 00:24:28 +0300

    Split dataset generation and model training

--- emotenn/constants.py
+++ emotenn/constants.py
@@ -17,6 +17,9 @@ GDriveFile = namedtuple('GDriveFile', ['id', 'path'])
 
 FER_ARCHIVE = GDriveFile('1Syp3_xi0rV_DlWYQP4KVYmfH9f4djtzT', os.path.join(DATASETS_DIR, 'fer2013.zip'))
 FER2013 = GDriveFile('1MdNY-bRxCg0oWnOec69rSRKTU_iUbEHv', os.path.join(DATASETS_DIR, 'fer2013.bin'))
+RAVDESS_SPEECH = GDriveFile('1b5Xq1whO2GPGekmJQe6AZOYIsiQCbMFq', os.path.join(DATASETS_DIR, 'Audio_Speech_Actors_01-24.zip'))
+RAVDESS_SONG = GDriveFile('1bS64Cj3eji6wD4C6nw1kyGlNZqUuy8P2', os.path.join(DATASETS_DIR, 'Audio_Song_Actors_01-24.zip'))
+RAVDESS = GDriveFile('1sJ1RNEpc8_VUcJ8yQ7UOGi4EyhTBH05f', os.path.join(DATASETS_DIR, 'ravdess.bin'))
 
 LU_CNN = GDriveFile('1RHaMKknF6ues_QPqxpQsXCrFRNScBbOA', os.path.join(TRAINED_MODELS_DIR, 'lu_cnn.h5'))
 LU_CNN_HISTORY = GDriveFile('1M4up-LLmMMRaq7XDlrD8CpermiOZrWg8', os.path.join(TRAINED_MODELS_DIR, 'lu_cnn_history.pkl'))
